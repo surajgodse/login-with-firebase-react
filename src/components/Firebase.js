28caf2a3175c166e85cb8aef8065ec0a5ec43c85
Suraj Godse <godsesuraj2140@gmail.com>
2024-08-14 00:19:31 -0500

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1_eCQqnznB_9YgLABts1UyrRc9NzBKCk",
-  authDomain: "login-auth-f1775.firebaseapp.com",
-  projectId: "login-auth-f1775",
-  storageBucket: "login-auth-f1775.appspot.com",
+  apiKey: "*******",
+  authDomain: "*****",
+  projectId: "************",
+  storageBucket: "**************",
   messagingSenderId: "320384479194",
-  appId: "1:320384479194:web:0363650d3620acd5149ae3"
+  appId: "*********************"
 };
 
 // Initialize Firebase
